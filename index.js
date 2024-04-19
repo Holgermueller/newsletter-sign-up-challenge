3c4d83b4c0eedb47114979bb56f07bd809532cb0
holgermueller <9jholger@gmail.com>
2024-04-19 15:46:32 -0500

    added form validation

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 "use strict";
+
+const validateForm = (event) => {
+  event.preventDefault();
+
+  let emailValue = document.getElementById("emailInput").value;
+  let reg = /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/;
+  let result = reg.test(emailValue);
+
+  if (result == false) {
+    console.log("failure");
+    document.getElementById("feedback").style.display = "block";
+
+    document.getElementById("emailInput").style.borderColor =
+      "hsl(4, 100%, 67%)";
+    document.getElementById("emailInput").style.backgroundColor =
+      "hsla(4, 100%, 67%, .2)";
+    document.getElementById("emailInput").style.color = "hsl(4, 100%, 67%)";
+  } else {
+    console.log("success");
+  }
+};
